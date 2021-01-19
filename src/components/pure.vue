f856027bc45c4340182fc29576ec26870c254d80
chenrrrrrr <cbetforvalue@gmail.com>
2021-01-20 05:10:13 +0800

    update:yaml

--- src/components/pure.vue
+++ src/components/pure.vue
@@ -6,72 +6,78 @@
       <div class="basic">
         <section v-if="d.basic">
           <h1 class="left">
-            {{d.basic.cnName}}
-            <small>{{d.basic.enName}}</small>
+            {{ d.basic.cnName }}
+            <small>{{ d.basic.enName }}</small>
           </h1>
           <h2 class="right">
-            {{renderData.basic.objective}}
-            <small>/ {{renderData.basic.city}}</small>
+            {{ renderData.basic.objective }}
+            <small>/ {{ renderData.basic.city }}</small>
           </h2>
         </section>
         <div class="relative" v-if="d.basic">
           <div class="left">
-            <div class="age">{{renderData.basic.sex}} / {{renderData.basic.birth}}</div>
-            <div class="overall">{{renderData.basic.overall}}</div>
-            <div class="college">{{renderData.basic.college}}</div>
+            <div class="age">
+              {{ renderData.basic.sex }} / {{ renderData.basic.birth }}
+            </div>
+            <div class="overall">{{ renderData.basic.overall }}</div>
+            <div class="college">{{ renderData.basic.college }}</div>
           </div>
           <div class="right">
             <a :href="`tel:${renderData.basic.phone}`">
-              <span>{{renderData.basic.phone}}</span>
+              <span>{{ renderData.basic.phone }}</span>
               <i class="fa fa-phone-square"></i>
             </a>
             <a :href="`https://www.${renderData.basic.git}`">
-              <span>{{renderData.basic.git}}</span>
+              <span>{{ renderData.basic.git }}</span>
               <i class="fa fa-git-square"></i>
             </a>
             <a :href="`https://www.${renderData.basic.homepage}`">
-              <span>{{renderData.basic.homepage}}</span>
+              <span>{{ renderData.basic.homepage }}</span>
               <i class="fa fa-h-square"></i>
             </a>
             <a :href="`Mailto:${renderData.basic.email}`">
-              <span>{{renderData.basic.email}}</span>
+              <span>{{ renderData.basic.email }}</span>
               <i class="fa fa-envelope-square"></i>
             </a>
           </div>
         </div>
       </div>
       <div class="skill" v-if="d.skill">
-        <h5>{{d.conf.lv.l1}}</h5>
+        <h5>{{ d.conf.lv.l1 }}</h5>
         <ul>
-          <li v-for="item in d.skill" :key="item">{{item}}</li>
+          <li v-for="item in d.skill" :key="item">{{ item }}</li>
         </ul>
       </div>
       <div class="exp" v-if="d.exp">
-        <h5>{{d.conf.lv.l2}}</h5>
+        <h5>{{ d.conf.lv.l2 }}</h5>
         <ul>
-          <li v-for="(item,index) in d.exp" :key="index">
+          <li v-for="(item, index) in d.exp" :key="index">
             <div class="title">
-              <span class="name">{{item.name}}</span>
-              <a v-if="item.link" :href="item.link" target="_blank" class="link">link</a>
+              <span class="name">{{ item.name }}</span>
+              <a v-if="item.link" :href="item.link" target="_blank" class="link"
+                >link</a
+              >
             </div>
             <div class="des">
-              <b>{{d.conf.lv.l4}}：</b>
+              <b>{{ d.conf.lv.l4 }}：</b>
               <span v-html="inlineCompiler(item.des.join(''))"></span>
             </div>
             <div class="stack" v-if="item.stack">
-              <b>{{d.conf.lv.l5}}：</b>
+              <b>{{ d.conf.lv.l5 }}：</b>
               <span v-html="stackCompiler(item.stack)"></span>
             </div>
           </li>
         </ul>
       </div>
       <div class="project" v-if="d.project">
-        <h5>{{d.conf.lv.l3}}</h5>
+        <h5>{{ d.conf.lv.l3 }}</h5>
         <ul>
-          <li v-for="(item,index) in d.project" :key="index">
-            <a :href="item.link" target="_blank" class="name">{{item.title}}</a>
+          <li v-for="(item, index) in d.project" :key="index">
+            <a :href="item.link" target="_blank" class="name">{{
+              item.title
+            }}</a>
             <span>：</span>
-            <span>{{item.des}}</span>
+            <span>{{ item.des }}</span>
           </li>
         </ul>
       </div>
@@ -88,7 +94,7 @@ export default {
   components: { loading },
   computed: {
     stackCompiler() {
-      return str => {
+      return (str) => {
         let list = str.split(" ");
         list.forEach((item, index) => {
           list[index] = `<label class='tip'>${item}</label>`;
@@ -98,11 +104,11 @@ export default {
     },
     inlineCompiler() {
       let re = /`{1,2}[^`](.*?)`{1,2}/g;
-      return str => {
+      return (str) => {
         let res = str;
         if (str.match(re)) {
           let temp = res;
-          str.match(re).forEach(item => {
+          str.match(re).forEach((item) => {
             temp = temp.replace(
               new RegExp(item, "g"),
               `<label class='tip-grey'>${item.replace(/`/g, "")}</label>`
@@ -112,7 +118,7 @@ export default {
         }
         return res;
       };
-    }
+    },
   },
   watch: {
     renderData: {
@@ -121,20 +127,20 @@ export default {
         // 隐藏loading
         this.loadingShow = false;
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   data() {
     return {
       d: {},
-      loadingShow: true
+      loadingShow: true,
     };
   },
   props: {
     renderData: {
-      type: Object
-    }
-  }
+      type: Object,
+    },
+  },
 };
 </script>
 
